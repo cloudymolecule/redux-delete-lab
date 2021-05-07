5605c9430b8d37751016ce1c63bf035ce0c2eaa9
Cloudymolecule <baires82@gmail.com>
2021-05-07 10:44:36 -0400

    Done.

---
+++ src/components/Bands.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Band from './Band'
+
+export default class Bands extends Component {
+    
+    
+    render() {
+        return (
+            <div>
+                {/* {console.log(this.props.bands)} */}
+                {this.props.bands.map(band => <Band key={band.id} id={band.id} name={band.name} deleteBand={this.props.deleteBand}/>)}
+            </div>
+        )
+    }
+}
